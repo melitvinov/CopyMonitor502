bf67097a9139a812e0ec8509a1ad2e1b2f9ead30
Max <melitvinov@fito-system.ru>
2017-02-15 15:06:46 +0300

    midl result

--- GGraf.pas
+++ GGraf.pas
@@ -11,7 +11,7 @@ uses
   TSGrid, BubbleCh, AppEvnts, GIFDef, GIFComponent,PaswordDlg, FPicLabel,
   CellChart;    //Port,SetGrid,
 
-const MesTest='M_01_00_39';
+const MesTest='M_01_00_40';
 type
   TFMain = class(TForm)
     MM1: TMainMenu;
